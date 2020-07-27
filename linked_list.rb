c6263af5dec719614faee333a9b8b48f8b7794c9
Timothy <tim87henry@gmail.com>
2020-07-27 22:24:56 +1000

    Added options to insert at and remove from index

--- linked_list.rb
+++ linked_list.rb
@@ -97,6 +97,51 @@ class LinkedList
         return string
     end
 
+    def insert_at(value, index)
+        if index>@@nodes.length
+          puts "The index is out of range"
+          return
+        end
+        node=Node.new(value)
+        temp_nodes=[]
+        for i in 0..index-1
+            temp_nodes[i]=@@nodes[i]
+        end
+        temp_nodes[index]=node
+        for i in index..@@nodes.length-1
+            temp_nodes[i+1]=@@nodes[i]
+        end
+        if index<@@nodes.length
+          temp_nodes[index].next_node=temp_nodes[index+1]
+        else
+          temp_nodes[index].next_node=nil
+        end
+        unless index==0
+          temp_nodes[index-1].next_node=node
+        end
+        @@nodes=temp_nodes
+    end
+  
+    def remove_at(index)
+        if index>=@@nodes.length
+          puts "The index is out of range"
+          return
+        end
+        temp_nodes=[]
+        for i in 0..index-1
+          temp_nodes[i]=@@nodes[i]
+        end
+        for i in index..@@nodes.length-2
+          temp_nodes[i]=@@nodes[i+1]
+        end
+        if index=@@nodes.length-1
+          temp_nodes[temp_nodes.length-1].next_node=nil
+        elsif index>0
+          temp_nodes[index-1].next_node=temp_nodes[index]
+        end
+        @@nodes=temp_nodes
+    end
+
 end
 
 class Node
@@ -106,18 +151,16 @@ class Node
         @value = value
         @next_node = nil
     end
-
-
-
 end
 
 llist=LinkedList.new()
-
+puts "Added three elements 44,55 and 66"
 llist.append(44)
 llist.append(55)
 llist.append(66)
 puts llist
 
+puts "Prepended element 99"
 llist.prepend(99)
 puts llist
 puts "Size of the linked list is #{llist.size}"
@@ -125,10 +168,23 @@ llist.head
 llist.tail
 llist.at(2)
 
+puts "Appending element 77"
 llist.append(77)
 puts llist
+puts "Popping element 77"
 llist.pop
 puts llist
 puts "Does it contain 77? #{llist.contains?(77)}"
 puts "Does it contain 55? #{llist.contains?(55)}"
 puts "Index of 55 is #{llist.find(55)}"
+
+puts "Inserting element 88 at index 7"
+llist.insert_at(88,7)
+puts llist
+puts "Inserting element 88 at index 4"
+llist.insert_at(88,4)
+puts llist
+
+puts "Removing element at index 0"
+llist.remove_at(0)
+puts llist
